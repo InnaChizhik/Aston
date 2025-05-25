01eeca6b8315adacac0b19c501b235879b216e3c
InnaChizhik <inna.chizhik.03.02@mail.ru>
2025-05-25 19:31:30 +0300

    Initial commit

--- src/main/java/Lesson_4/Main.java
+++ src/main/java/Lesson_4/Main.java
@@ -7,8 +7,13 @@ public class Main {
         CollectionStudent studentCollection = new CollectionStudent();
         studentCollection.addStudents(new Student("Артур", 202, 2, Map.of("Алгебра ", 8, "История", 9, "Геометрия ", 9)));
         studentCollection.addStudents(new Student("Павел", 202, 2, Map.of("Алгебра ", 7, "История", 7, "Геометрия ", 7)));
-        studentCollection.addStudents(new Student("Иван", 202, 2, Map.of("Алгебра ", 1, "История", 2, "Геометрия ", 3)));
+        studentCollection.addStudents(new Student("Иван", 202, 2, Map.of("Алгебра ", 1, "История", 2, "Геометрия ", 1)));
         studentCollection.addStudents(new Student("Мария", 202, 1, Map.of("Алгебра ", 9, "История", 8, "Геометрия ", 8)));
         studentCollection.printStudents(2);
+        studentCollection.perevodStudents();
+        studentCollection.printStudents(2);
+        studentCollection.removeStudents();
+        studentCollection.printStudents(2);
+
     }
 }
