3a4985f71502b9416d2b0aeab3d706ce40553044
InnaChizhik <inna.chizhik.03.02@mail.ru>
2025-05-25 19:28:10 +0300

    Initial commit

--- src/main/java/Lesson_4/Main.java
+++ src/main/java/Lesson_4/Main.java
@@ -8,6 +8,7 @@ public class Main {
         studentCollection.addStudents(new Student("Артур", 202, 2, Map.of("Алгебра ", 8, "История", 9, "Геометрия ", 9)));
         studentCollection.addStudents(new Student("Павел", 202, 2, Map.of("Алгебра ", 7, "История", 7, "Геометрия ", 7)));
         studentCollection.addStudents(new Student("Иван", 202, 2, Map.of("Алгебра ", 1, "История", 2, "Геометрия ", 3)));
+        studentCollection.addStudents(new Student("Мария", 202, 1, Map.of("Алгебра ", 9, "История", 8, "Геометрия ", 8)));
         studentCollection.printStudents(2);
     }
 }
